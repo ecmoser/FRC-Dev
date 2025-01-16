e65c026906b5b4c2127bb9a18a790f65b18e716c
ecmoser <78171469+ecmoser@users.noreply.github.com>
2025-01-16 13:15:20 -0500

    Note what values need to be changed

--- src/main/java/frc/robot/subsystems/drive/DriveConstants.java
+++ src/main/java/frc/robot/subsystems/drive/DriveConstants.java
@@ -28,8 +28,8 @@ import org.ironmaple.simulation.drivesims.configs.SwerveModuleSimulationConfig;
 public class DriveConstants {
     public static final double maxSpeedMetersPerSec = 4.8;
     public static final double odometryFrequency = 100.0; // Hz
-    public static final double trackWidth = Units.inchesToMeters(26.5);
-    public static final double wheelBase = Units.inchesToMeters(26.5);
+    public static final double trackWidth = Units.inchesToMeters(26.5); //FIXME
+    public static final double wheelBase = Units.inchesToMeters(26.5); //FIXME
     public static final double driveBaseRadius = Math.hypot(trackWidth / 2.0, wheelBase / 2.0);
     public static final Translation2d[] moduleTranslations = new Translation2d[] {
         new Translation2d(trackWidth / 2.0, wheelBase / 2.0),
@@ -39,29 +39,29 @@ public class DriveConstants {
     };
 
     // Zeroed rotation values for each module, see setup instructions
-    public static final Rotation2d frontLeftZeroRotation = new Rotation2d(0.0);
-    public static final Rotation2d frontRightZeroRotation = new Rotation2d(0.0);
-    public static final Rotation2d backLeftZeroRotation = new Rotation2d(0.0);
-    public static final Rotation2d backRightZeroRotation = new Rotation2d(0.0);
+    public static final Rotation2d frontLeftZeroRotation = new Rotation2d(0.0); //FIXME
+    public static final Rotation2d frontRightZeroRotation = new Rotation2d(0.0); //FIXME
+    public static final Rotation2d backLeftZeroRotation = new Rotation2d(0.0); //FIXME
+    public static final Rotation2d backRightZeroRotation = new Rotation2d(0.0); //FIXME
 
     // Device CAN IDs
     public static final int pigeonCanId = 9;
 
-    public static final int frontLeftDriveCanId = 1;
-    public static final int backLeftDriveCanId = 3;
-    public static final int frontRightDriveCanId = 5;
-    public static final int backRightDriveCanId = 7;
+    public static final int frontLeftDriveCanId = 1; //FIXME
+    public static final int backLeftDriveCanId = 3; //FIXME
+    public static final int frontRightDriveCanId = 5; //FIXME
+    public static final int backRightDriveCanId = 7; //FIXME
 
-    public static final int frontLeftTurnCanId = 2;
-    public static final int backLeftTurnCanId = 4;
-    public static final int frontRightTurnCanId = 6;
-    public static final int backRightTurnCanId = 8;
+    public static final int frontLeftTurnCanId = 2; //FIXME
+    public static final int backLeftTurnCanId = 4; //FIXME
+    public static final int frontRightTurnCanId = 6; //FIXME
+    public static final int backRightTurnCanId = 8; //FIXME
 
     // Drive motor configuration
-    public static final int driveMotorCurrentLimit = 60;
-    public static final double wheelRadiusMeters = Units.inchesToMeters(1.5);
+    public static final int driveMotorCurrentLimit = 60; //FIXME
+    public static final double wheelRadiusMeters = Units.inchesToMeters(1.5); //FIXME
     public static final double driveMotorReduction =
-            (45.0 * 22.0) / (14.0 * 15.0); // MAXSwerve with 14 pinion teeth and 22 spur teeth
+            (45.0 * 22.0) / (14.0 * 15.0); // MAXSwerve with 14 pinion teeth and 22 spur teeth //FIXME
     public static final DCMotor driveGearbox = DCMotor.getNeoVortex(1);
 
     // Drive encoder configuration
@@ -71,38 +71,38 @@ public class DriveConstants {
             (2 * Math.PI) / 60.0 / driveMotorReduction; // Rotor RPM -> Wheel Rad/Sec
 
     // Drive PID configuration
-    public static final double driveKp = 0.0;
-    public static final double driveKd = 0.0;
-    public static final double driveKs = 0.0;
-    public static final double driveKv = 0.1;
+    public static final double driveKp = 0.0; //FIXME
+    public static final double driveKd = 0.0; //FIXME
+    public static final double driveKs = 0.0; //FIXME
+    public static final double driveKv = 0.1; 
     public static final double driveSimP = 0.05;
     public static final double driveSimD = 0.0;
     public static final double driveSimKs = 0.0;
     public static final double driveSimKv = 0.0789;
 
     // Turn motor configuration
-    public static final boolean turnInverted = false;
+    public static final boolean turnInverted = false; //FIXME
     public static final int turnMotorCurrentLimit = 20;
     public static final double turnMotorReduction = 9424.0 / 203.0;
-    public static final DCMotor turnGearbox = DCMotor.getNeo550(1);
+    public static final DCMotor turnGearbox = DCMotor.getNeo550(1); //FIXME
 
     // Turn encoder configuration
-    public static final boolean turnEncoderInverted = true;
+    public static final boolean turnEncoderInverted = true; //FIXME
     public static final double turnEncoderPositionFactor = 2 * Math.PI; // Rotations -> Radians
     public static final double turnEncoderVelocityFactor = (2 * Math.PI) / 60.0; // RPM -> Rad/Sec
 
     // Turn PID configuration
-    public static final double turnKp = 2.0;
-    public static final double turnKd = 0.0;
-    public static final double turnSimP = 8.0;
-    public static final double turnSimD = 0.0;
+    public static final double turnKp = 2.0; //FIXME
+    public static final double turnKd = 0.0; //FIXME
+    public static final double turnSimP = 8.0; //FIXME
+    public static final double turnSimD = 0.0; //FIXME
     public static final double turnPIDMinInput = 0; // Radians
     public static final double turnPIDMaxInput = 2 * Math.PI; // Radians
 
     // PathPlanner configuration
-    public static final double robotMassKg = 45;
-    public static final double robotMOI = 6.883;
-    public static final double wheelCOF = 1.2;
+    public static final double robotMassKg = 45; //FIXME
+    public static final double robotMOI = 6.883; //FIXME
+    public static final double wheelCOF = 1.2; //FIXME
     public static final RobotConfig ppConfig = new RobotConfig(
             robotMassKg,
             robotMOI,
